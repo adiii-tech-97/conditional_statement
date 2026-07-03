ff8a88eeea195803baddec4ba48b69484ab720c0
adityawagh107-skech <adityawagh107@gmail.com>
2026-07-03 14:43:44 +0530

    conditional statement

---
+++ if_statement.py
@@ -0,0 +1,43 @@
+print("if_statement")
+
+age=int(input("enter your age"))
+if age >= 18:
+    print("person eigible to vote")
+
+
+    number = int(input("enter your number"))
+    if number > 0:
+         print("positive number")
+
+         
+         marks = int(input("enter your marks"))
+         if marks >= 90:
+              print("out of 90 marks")
+
+
+
+              stock = int(input("enter your product"))
+              if stock >= 10:
+                   print("product in stock ")
+
+
+                   user_age = int(input("enter your age"))
+                   if user_age >= 18:
+                        print("user age is greater to 18")
+
+
+                        temp =int(input("enter temp"))
+                        if temp >= 100:
+                             print("temp is high")
+
+
+                             sallory = int(input("enter your sallory"))
+                             if sallory >= 50000:
+                                  print("sallory is greater to 50000")
+
+
+
+
+
+
+
