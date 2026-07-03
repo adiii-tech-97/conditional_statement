026a68e18f3e616ba144b45b27cb407d807c9fce
adityawagh107-skech <adityawagh107@gmail.com>
2026-07-03 19:44:01 +0530

    add new update

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,11 +1,18 @@
 print("if_else_statement")
 
 
+
+print("-----------------")
+
 number = -40
 if number >= 0:
     print("positive number")
 else:
     print("negative number")
+
+
+print("-----------------")
+
 
 
 number = 22
@@ -14,11 +21,22 @@ if number >= 9:
 else:
     print("odd number")
 
+
+print("-----------------")
+
+
+
 age = 16
 if age >= 18:
     print("person eligible to vote")
 else:
     print("person not eligible to vote")
+
+    print("-----------------")
+
+
+
+
 
 
 marks = 33
@@ -28,11 +46,20 @@ else:
     print("student failed")
 
 
+
+print("-----------------")
+
+
+
+
 balance = 1000
 if balance >= 1500:
     print("payment done")
 else:
     print("payment failed")
+
+print("-----------------")
+
 
 
 stock = 14
@@ -40,6 +67,9 @@ if stock >= 20:
     print("product available")
 else:
     print("product is not available")
+
+    print("-----------------")
+    
 
 temp = 45
 if temp >= 40:
--- if_statement.py
+++ if_statement.py
@@ -1,18 +1,30 @@
 print("if_statement")
 
+
+print("-----------------")
+
+
 age=int(input("enter your age"))
 if age >= 18:
     print("person eigible to vote")
+
+    print("-----------------")
+
 
 
     number = int(input("enter your number"))
     if number > 0:
          print("positive number")
 
-         
-         marks = int(input("enter your marks"))
-         if marks >= 90:
+
+print("-----------------")
+
+
+          marks = int(input("enter your marks"))
+          if marks >= 90:
               print("out of 90 marks")
+
+              print("-----------------")
 
 
 
@@ -20,15 +32,24 @@ if age >= 18:
               if stock >= 10:
                    print("product in stock ")
 
+                   print("-----------------")
+
+
+
 
                    user_age = int(input("enter your age"))
                    if user_age >= 18:
                         print("user age is greater to 18")
 
 
-                        temp =int(input("enter temp"))
+print("-----------------")
+
+
+                       temp =int(input("enter temp"))
                         if temp >= 100:
                              print("temp is high")
+
+                             print("-----------------")
 
 
                              sallory = int(input("enter your sallory"))
@@ -36,7 +57,7 @@ if age >= 18:
                                   print("sallory is greater to 50000")
 
 
-
+print("-----------------")
 
 
 
